84c907c46ac0fac75d5b47e846db140ad18ae8f5
Christopher Enam Franklin <cef1911@hotmail.com>
2024-06-13 19:59:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ from PIL import Image
 # Hi Everyone
 """
 
-
+st.title('Test')
 st.subheader('What a pleasure it was on Saturday June 8, 2024 Fishing and Fellowshipping', divider='rainbow')
 st.subheader('_Fishing with Friends and Family_ is :blue[cool] :sunglasses:')
 
