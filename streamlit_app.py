3c6e585c32b2383473205d3ba363f756bbe61c0c
jesseb347 <159094658+jesseb347@users.noreply.github.com>
2024-07-18 18:51:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,8 @@ from PIL import Image
 
 
 """
-# Draft of June 8, 2024 Kids Fishing Data Science Project and Activity
-# Teams on the Fishing Trip
-5 teams with 2 Kids each team
+# Draft of Jesse Resume!
+
 
 """
 
